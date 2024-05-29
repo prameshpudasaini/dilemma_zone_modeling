8d3a05d9c9c38736a2879c5cb4becaf508d84cf0
prameshpudasaini <prameshpudasaini@gmail.com>
2024-05-29 12:45:08 -0700

    Updated for monthly trip extraction

--- script/test_Speedway_Campbell/extract_trips.py
+++ script/test_Speedway_Campbell/extract_trips.py
@@ -2,47 +2,37 @@
 
 import os
 import pandas as pd
-import dask.dataframe as dd
 
-os.chdir(r"E:\Wejo\Wejo-Aug-Nov 2021")
+os.chdir(r"D:\GitHub\dilemma_Wejo")
 
-# read Wejo trajectory data for August 17, 2021
-file_input = "output_dt=2021-08-17.csv"
-df = dd.read_csv(file_input, assume_missing = True)
+# list of Wejo trajectory data
+wejo_path = os.path.join("script/test_Speedway_Campbell/data/Wejo_Aug_Nov")
+files_wejo = os.listdir(wejo_path)
 
-# get unique trip IDs
-all_trips = set(df.TripID.unique())
+# specify start and end times to filter dataset for October
+start_time, end_time = '2021-10-01 00:00:00', '2021-11-01 00:00:00'
 
-# specify coordinate gates for filtering trip trajectories
-gate = {'g1': {'c1': [32.23630647990695, -110.93969748930743], 'c2': [32.235866420981864, -110.94095037095649]},
-        'g2': {'c1': [32.236272521830756, -110.94338137745342], 'c2': [32.23588077273965, -110.94462299506206]},
-        'g3': {'c1': [32.236248338346755, -110.9465362446941], 'c2': [32.235817764157865, -110.9477971426006]}}
+ldf = [] # store filtered df
 
-# store trip IDs passing through coordinate gates for each approach
-gate_trips = []
-
-for k in gate.keys():
-    # create list of lat and lng for gate k
-    lat = [gate[k]['c1'][0], gate[k]['c2'][0]]
-    lng = [gate[k]['c1'][1], gate[k]['c2'][1]]
+# loop through each file and filter data start & end times
+for file in files_wejo:
+    xdf = pd.read_csv(os.path.join(wejo_path, file))
     
-    # filter trip IDs passing over gate k and store the result
-    trip_k = set(df[(df.Latitude.between(min(lat), max(lat))) & (df.Longitude.between(min(lng), max(lng)))].TripID.unique())
-    gate_trips.append(trip_k)
-    
-# intersection of sets gives IDs of common trips passing through all gates
-trips = sorted(list(set.intersection(all_trips, *gate_trips)))
+    # convert localtime to datetime format
+    xdf.localtime = pd.to_datetime(xdf.localtime)
 
-# filter df for trip IDs passing through all gates
-gdf = df.copy()[df.TripID.isin(trips)]
+    # filter trips between start and end times
+    xdf = xdf[xdf.localtime.between(start_time, end_time, inclusive = 'left')]
 
-# specify min-max lat and lng bounds
-lat = [gate['g1']['c1'][0], gate['g3']['c2'][0]]
-lng = [gate['g1']['c1'][1], gate['g3']['c2'][1]]
+    # add df to list
+    ldf += [xdf]
 
-# filter trips within these bounds
-gdf = gdf[(gdf.Latitude.between(min(lat), max(lat))) & (gdf.Longitude.between(min(lng), max(lng)))].compute()
+# combine list of df
+df = pd.concat(ldf, ignore_index = True)
+
+# remove redundant columns
+df.drop(['Altitude', 'unixtime', 'accuracy'], axis = 1, inplace = True)
 
 # save file
-file_output = os.path.join(r"C:\Users\pramesh\Wejo\DZ_test", file_input)
-gdf.to_csv(file_output, index = False)
+output_path = os.path.join("script/test_Speedway_Campbell/data", "Speedway_Campbell_Oct.txt")
+df.to_csv(output_path, index = False, sep = '\t')
