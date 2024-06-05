0a2d2e91bd36653c37f2fcf411f3da780e81a121
prameshpudasaini <prameshpudasaini@gmail.com>
2024-06-05 10:40:36 -0700

    Updated for individual trip

--- script/test_Speedway_Campbell/visualize_trip.R
+++ script/test_Speedway_Campbell/visualize_trip.R
@@ -1,54 +1,39 @@
 library(data.table)
+library(lubridate)
 library(leaflet)
 
-# function to round direction to nearest 0, 90, 180, 270, 360
-round_direction <- function(direction) {
-    target_values = c(0, 90, 180, 270, 360)
-    
-    # find the closest target value for each direction
-    sapply(direction, function(x) {
-        target_values[which.min(abs(target_values - x))]
-    })
-}
+# file paths
+file_wejo_traj <- "script/test_Speedway_Campbell/data/Wejo/Speedway_Campbell_08.txt"
+file_trip_times <- "script/test_Speedway_Campbell/data/Wejo/Speedway_Campbell_08_trip_times.txt"
 
 # read data
-DT <- fread("script/test_Speedway_Campbell/data/output_dt=2021-08-17.csv")
+wdf <- fread(file_wejo_traj, sep = '\t')
+tdf <- fread(file_trip_times, sep = '\t')
 
-# count number of unique trips
-length(unique(DT$TripID))
+# add day column to wejo data
+wdf[, Day := mday(localtime)]
 
-# summarize direction of trips
-summary(DT$direction)
+plotTrajectory <- function(day, trip_id) {
+    df <- copy(wdf)[Day == day & TripID == trip_id]
+    
+    fig <- leaflet(df) |> 
+        addTiles() |> 
+        addCircleMarkers(
+            lng = ~Longitude, 
+            lat = ~Latitude,
+            radius = 5,
+            label = ~LocationID
+        )
+    
+    return(fig)
+}
 
-# compute average and standard deviation of each trip's direction
-DT[, dirc_avg := round(mean(direction), 0), by = .(TripID)]
-DT[, dirc_sd := round(sd(direction), 2), by = .(TripID)]
+plotTrajectory(4, 129781)
+plotTrajectory(5, 129781)
+plotTrajectory(10, 40641)
+plotTrajectory(1, 127286)
 
-# summarize average and standard deviation of each trip's direction
-summary(DT$dirc_avg)
-summary(DT$dirc_sd)
-
-# visualize average and standard deviation of each trip's direction
-plot(DT$dirc_avg, DT$dirc_sd)
-
-# filter trips with standard deviation of trip direction < 2
-DT <- DT[dirc_sd <= 2, ]
-plot(DT$dirc_avg, DT$dirc_sd)
-
-# count number of unique trips after filtering
-length(unique(DT$TripID))
-
-# round average direction and update direction
-DT[, direction := round_direction(dirc_avg)]
-
-# visualize trip
-df <- copy(DT)[TripID == 219]
-
-leaflet(df) |> 
-    addTiles() |> 
-    addCircleMarkers(
-        lng = ~Longitude, 
-        lat = ~Latitude,
-        radius = 5,
-        label = ~LocationID
-    )
+plotTrajectory(14, 26501)
+plotTrajectory(21, 162968)
+plotTrajectory(6, 24941)
+plotTrajectory(23, 114253)
