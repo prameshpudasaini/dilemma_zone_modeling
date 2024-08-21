9f3f36bf13f0c98ee0e5c2996769bf9abbee257b
prameshpudasaini <prameshpudasaini@gmail.com>
2024-08-21 11:31:41 -0700

    Compiled raw trajectory data

--- script/data_preprocessing/compile_raw_trajectory_data.py
+++ script/data_preprocessing/compile_raw_trajectory_data.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-os.chdir(r"D:\GitHub\dilemma_Wejo")
+os.chdir(r"D:\GitHub\dilemma_zone_modeling")
 
 # path and folders with raw Wejo trajectory data
 wejo_path = os.path.join("ignore/Wejo/raw_data")
@@ -14,14 +14,14 @@ for folder in wejo_folders:
     # list of files with Wejo trajectory data
     wejo_files = os.listdir(os.path.join(wejo_path, folder))
     
-    month = 8 # month to filter data for
+    months = [8, 9, 10] # month to filter data for
     ldf = [] # store filtered df
 
     # loop through each file and filter data for month
     for file in wejo_files:
         xdf = pd.read_csv(os.path.join(wejo_path, folder, file))
         xdf.localtime = pd.to_datetime(xdf.localtime) # convert localtime to datetime format
-        xdf = xdf[xdf.localtime.dt.month == month]
+        xdf = xdf[xdf.localtime.dt.month.isin(months)]
         ldf.append(xdf) # add df to list
 
     # combine list of df
