dcfff03343e2d329ab547f626aa43444409287d9
prameshpudasaini <prameshpudasaini@gmail.com>
2024-08-26 14:33:38 -0700

    Minor update for extracting stop/go trips

--- script/trips_processing/extract_stop_go_trips.py
+++ script/trips_processing/extract_stop_go_trips.py
@@ -1,16 +1,20 @@
 import os
 import pandas as pd
 
-os.chdir(r"D:\GitHub\dilemma_Wejo")
+os.chdir(r"D:\GitHub\dilemma_zone_modeling")
 
 # path and list of files
-file_path = "ignore/Wejo/processed_data"
+file_path = "ignore/Wejo/processed_trips"
 file_list = os.listdir(file_path)
 
 list_df = [] # initialize empty list to store 
 # loop through each file and append to list
 for file in file_list:
+    print(f"Processing file: {file}")
     file_df = pd.read_csv(os.path.join(file_path, file), sep = '\t')
+    
+    # filter out Queued, Stopped trips and drop redundant columns
+    file_df = file_df[~file_df.Group.isin(['Queued', 'Stopped'])]
     
     # add node and approach as variables
     node, dirc = file[:3], file[4:6]
@@ -22,13 +26,6 @@ for file in file_list:
 # combine list into a single df    
 df = pd.concat(list_df, ignore_index = True)
 
-# update localtime to pandas datetime and add day variable
-df.localtime = pd.to_datetime(df.localtime)
-df['Day'] = df.localtime.dt.day
-
-# filter out Queued, Stopped trips and drop redundant columns
-df = df[~df.Group.isin(['Queued', 'Stopped'])]
-
 # update columns order and save file
-df = df[['Node', 'Approach', 'ID', 'TripID', 'localtime', 'speed', 'Xi', 'Signal', 'TUY', 'TAY', 'Group', 'Day']]
-df.to_csv("ignore/Wejo/trips_stop_go/trips_stop_go.txt", sep = '\t', index = False)
+df = df[['Node', 'Approach', 'ID', 'TripID', 'localtime', 'speed', 'Xi', 'Signal', 'TUY', 'TAY', 'Group']]
+df.to_csv("ignore/Wejo/trips_analysis/processed_trips_filtered.txt", sep = '\t', index = False)
